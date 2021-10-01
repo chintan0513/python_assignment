a0072f9c02a5a05afa76bb975470f0a1426223c9
chintan0513 <64206626+chintan0513@users.noreply.github.com>
2021-10-01 12:16:52 +0530

    Day 1 & 2 assignment submitted successfully :)

---
+++ pyton_assignment.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+x = 2
+y = 5
+xy = 'Hey'
+print (x+y, xy)
+
+
+# In[10]:
+
+
+print("""My name is Rajath Kumar M.P.
+
+I love Python.""")
+
+
+# In[8]:
+
+
+x = 10
+print(type(x))
+ 
+s = 'abc'
+print(type(s))
+ 
+value= True
+print(type(value))
+
+percent=90.00
+type(percent)
+
+
+# In[11]:
+
+
+Code:
+print("I want \\n to be printed.")
+
+
+# In[12]:
+
+
+string1 = 'World'
+print('Hello', string1)
+
+string2 = '!'
+print('Hello', string1, string2)
+
+
+# In[14]:
+
+
+print('Hello' + string1 + string2)
+
+
+# In[15]:
+
+
+print("Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug")
+
